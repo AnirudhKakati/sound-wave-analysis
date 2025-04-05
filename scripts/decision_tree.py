ff9f52b40c1acaa3e002e31724cc627276b40f7a
Anirudh Kakati <anirudhkakati@gmail.com>
2025-04-05 16:15:16 -0600

    added script for performing decision tree classification

--- scripts/decision_tree.py
+++ scripts/decision_tree.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
@@ -20,7 +21,7 @@ def get_data():
 
     Returns:
     - X (pd.DataFrame): Feature set excluding the label column.
-    - y (pd.Series): Binary target labels (1 for birds, 0 for dog_sounds).
+    - y (pd.Series): Binary target labels (1 for birds, 0 for dog_bark).
     """
     
     file_path="../audiofiles_processed_features_CSVs"
@@ -36,6 +37,53 @@ def get_data():
     X=df.drop("category",axis=1)
 
     return X,y
+
+def perform_decision_tree_classification():
+    """
+    Function to train and evaluate multiple Decision Tree classifiers with different configurations.
+
+    This function:
+    - Loads and prepares the dataset using `get_data`.
+    - Shuffles feature columns to simulate randomized selection.
+    - Trains three decision trees with different hyperparameters:
+        - Tree 1: Gini impurity, max depth = 3, max features = 10
+        - Tree 2: Log loss (cross-entropy), max depth = 2, max features = 5
+        - Tree 3: Entropy, max depth = 3
+    - Evaluates each tree using `plot_and_evaluate_tree` and saves performance visualizations.
+
+    Returns:
+    - None (Prints progress and saves all evaluation plots for each model)
+    """
+
+    X, y=get_data()
+    X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.3, random_state=18)
+
+    scaler=StandardScaler()
+    X_train_scaled=scaler.fit_transform(X_train)
+    X_test_scaled=scaler.transform(X_test)
+
+    # we preserve feature names for plotting the tree
+    features=list(X.columns)
+    X_train_scaled=pd.DataFrame(X_train_scaled, columns=features)
+    X_test_scaled=pd.DataFrame(X_test_scaled, columns=features)
+
+    random.seed(17)
+    random.shuffle(features)
+
+    #Tree 1: Gini (depth=3, features=10)")
+    tree1=DecisionTreeClassifier(max_depth=3, max_features=10,criterion="gini", random_state=18)
+    plot_and_evaluate_tree(X_train_scaled[features], X_test_scaled[features], y_train, y_test, tree1, "Tree 1: Gini (depth=3, features=10)")
+    print("Completed Tree 1")
+
+    #Tree 2: LogLoss (depth=2, features=5)
+    tree2=DecisionTreeClassifier(max_depth=2, criterion="log_loss", max_features=5, random_state=18)
+    plot_and_evaluate_tree(X_train_scaled[features], X_test_scaled[features], y_train, y_test, tree2, "Tree 2: LogLoss (depth=2, features=5)")
+    print("Completed Tree 2")
+
+    #Tree 3: Entropy (depth=3)
+    tree3=DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=18)
+    plot_and_evaluate_tree(X_train_scaled, X_test_scaled, y_train, y_test, tree3, "Tree 3: Entropy (depth=3)")
+    print("Completed Tree 3")
 
 def plot_and_evaluate_tree(X_train, X_test, y_train, y_test, model, title):
     """
@@ -79,7 +127,7 @@ def plot_and_evaluate_tree(X_train, X_test, y_train, y_test, model, title):
     report=classification_report(y_test, y_pred, output_dict=True)
     df_report=pd.DataFrame(report).T
     plt.figure(figsize=(8, 6))
-    sns.heatmap(df_report.iloc[:-1, :-1], annot=True, cmap="coolwarm", fmt=".4f")
+    sns.heatmap(df_report.iloc[:-1, :-1], annot=True, cmap="coolwarm", fmt=".3f")
 
     plt.title(f"Classification Report Heatmap - {title}", fontsize=11, fontweight='bold')
     plt.savefig(f"{output_path}/tree_{title.split()[1][:1]}_classification_report.png", dpi=200, bbox_inches='tight')
@@ -97,7 +145,7 @@ def plot_and_evaluate_tree(X_train, X_test, y_train, y_test, model, title):
     fpr,tpr,_=roc_curve(y_test, y_prob)
     roc_auc=auc(fpr, tpr)
     plt.figure(figsize=(8, 6))
-    plt.plot(fpr, tpr, color='royalblue', linewidth=2.5, label=f'AUC = {roc_auc:.2f}')
+    plt.plot(fpr, tpr, color='royalblue', linewidth=2.5, label=f'AUC = {roc_auc:.3f}')
     plt.plot([0, 1], [0, 1], linestyle='--', color='gray', linewidth=1.5)
 
     plt.xlabel("False Positive Rate", fontsize=12, fontweight='bold')
@@ -108,45 +156,6 @@ def plot_and_evaluate_tree(X_train, X_test, y_train, y_test, model, title):
     plt.tight_layout()
     plt.savefig(f"{output_path}/tree_{title.split()[1][:1]}_roc_curve.png", dpi=200, bbox_inches='tight')
     plt.close()
-
-def perform_decision_tree_classification():
-    """
-    Function to train and evaluate multiple Decision Tree classifiers with different configurations.
-
-    This function:
-    - Loads and prepares the dataset using `get_data`.
-    - Shuffles feature columns to simulate randomized selection.
-    - Trains three decision trees with different hyperparameters:
-        - Tree 1: Gini impurity, max depth = 3, max features = 10
-        - Tree 2: Log loss (cross-entropy), max depth = 2, max features = 5
-        - Tree 3: Entropy, max depth = 3
-    - Evaluates each tree using `plot_and_evaluate_tree` and saves performance visualizations.
-
-    Returns:
-    - None (Prints progress and saves all evaluation plots for each model)
-    """
-
-    X, y=get_data()
-    X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.3, random_state=18)
-
-    features=list(X.columns)
-    random.seed(17)
-    random.shuffle(features)
-
-    #Tree 1: Gini (depth=3, features=10)")
-    tree1=DecisionTreeClassifier(max_depth=3, max_features=10,criterion="gini", random_state=18)
-    plot_and_evaluate_tree(X_train, X_test, y_train, y_test, tree1, "Tree 1: Gini (depth=3, features=10)")
-    print("Completed Tree 1")
-
-    #Tree 2: LogLoss (depth=2, features=5)
-    tree2=DecisionTreeClassifier(max_depth=2, criterion="log_loss", max_features=5, random_state=18)
-    plot_and_evaluate_tree(X_train[features], X_test[features], y_train, y_test, tree2, "Tree 2: LogLoss (depth=2, features=5)")
-    print("Completed Tree 2")
-
-    #Tree 3: Entropy (depth=3)
-    tree3=DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=18)
-    plot_and_evaluate_tree(X_train, X_test, y_train, y_test, tree3, "Tree 3: Entropy (depth=3)")
-    print("Completed Tree 3")
 
 if __name__ == "__main__":
     perform_decision_tree_classification()
